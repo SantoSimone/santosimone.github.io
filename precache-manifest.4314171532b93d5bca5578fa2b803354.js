e969ad46b15cdc169e2ac505d69e206e2fd4679a
Simone Santillo <simone.santillo@gmail.com>
2021-06-13 16:31:46 +0200

    Cleaning up

---
+++ precache-manifest.4314171532b93d5bca5578fa2b803354.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3d4251ba47354b38557906767735dad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "233fdd9aef30a7356773",
+    "url": "/static/css/3.bc85b1b3.chunk.css"
+  },
+  {
+    "revision": "ffa37beee47503c645d0",
+    "url": "/static/css/4.5c57288c.chunk.css"
+  },
+  {
+    "revision": "544d81e7ba23bbd2d5f7",
+    "url": "/static/css/main.77ebf6ae.chunk.css"
+  },
+  {
+    "revision": "a686b15237ef18e16a46",
+    "url": "/static/js/2.5fb310c1.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/static/js/2.5fb310c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "233fdd9aef30a7356773",
+    "url": "/static/js/3.1d25f798.chunk.js"
+  },
+  {
+    "revision": "ffa37beee47503c645d0",
+    "url": "/static/js/4.71e068a0.chunk.js"
+  },
+  {
+    "revision": "544d81e7ba23bbd2d5f7",
+    "url": "/static/js/main.6adde57f.chunk.js"
+  },
+  {
+    "revision": "061152febe388a624597",
+    "url": "/static/js/runtime-main.75577ce4.js"
+  },
+  {
+    "revision": "a3752c4bf72688b3db38d0911cf75f61",
+    "url": "/static/media/Agustina.a3752c4b.woff"
+  },
+  {
+    "revision": "847627bffd52aab57ce2e903f144e07f",
+    "url": "/static/media/Montserrat-Regular.847627bf.ttf"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "5de3b11d1482849e4d8ed47b27c49e14",
+    "url": "/static/media/dvcLogo.5de3b11d.jpg"
+  },
+  {
+    "revision": "20cd539534801c7d2a6327e3cbd01ca4",
+    "url": "/static/media/loginLogo.20cd5395.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "492ec1c24ab013e66f887659b43db0a9",
+    "url": "/static/media/sportinsiemeLogo.492ec1c2.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "429f6287972e5c544879173feda1d409",
+    "url": "/static/media/unimoreLogo.429f6287.jpg"
+  }
+]);
